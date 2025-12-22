ef3832a71f5676e7eaee5a0b2504ce038947ec1d
Reginaldo Mota <reginaldomotacc@gmail.com>
2025-12-21 21:43:04 -0300

    feat: criação da base executável para o projeto

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeMetrics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
