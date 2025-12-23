8632ea03bc5e152e311d5a14427da7ff369ac09d
Reginaldo Mota <reginaldomotacc@gmail.com>
2025-12-22 22:53:37 -0300

    feat: implementação do SetOperations - Set (conjunto) - para a diferença entre dos dois conjuntos

---
+++ SetOperations.cs
@@ -0,0 +1,16 @@
+namespace CodeMetrics;
+
+public class SetOperations
+{
+    public static int[] Difference(int[] a, int[] b)
+    {
+        HashSet<int> setB = new HashSet<int>(b);
+        List<int> result = new List<int>();
+        
+        foreach (int item in a)
+            if(!setB.Contains(item))
+                result.Add(item);
+
+        return result.ToArray();
+    }
+}
