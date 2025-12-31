9edb5f1d466129173e088ae0708e38351fc27da5
Reginaldo Mota <reginaldomotacc@gmail.com>
2025-12-31 16:06:11 -0300

    refactor: remoção do execute para adequação de segregação de responsabilidades

--- Application/Operations/SetOperations.cs
+++ Application/Operations/SetOperations.cs
@@ -1,26 +1,10 @@
+using Domain.Interfaces;
+
 namespace Application.Operations;
 
-using Application.Operations.Utils;
-
-public class SetOperations
+public class SetOperations : ISetOperations
 {
-    public void Execute()
-    {
-        Console.Write("Digite o primeiro array (separado por vírgula): ");
-        string input = Console.ReadLine() ?? "";
-        int[] a = Parser.ParseArray(input);
-
-        Console.Write("Digite o segundo array (separado por vírgula): ");
-        input = Console.ReadLine() ?? "";
-        int[] b = Parser.ParseArray(input);
-        int[] result = Difference(a, b);
-
-        Console.WriteLine($"\nArray A: [{string.Join(", ", a)}]");
-        Console.WriteLine($"Array B: [{string.Join(", ", b)}]");
-        Console.WriteLine($"Diferença (A - B): [{string.Join(", ", result)}]");
-    }
-
-    private int[] Difference(int[] a, int[] b)
+    public int[] Difference(int[] a, int[] b)
     {
         HashSet<int> setB = new HashSet<int>(b);
         List<int> result = new List<int>();
