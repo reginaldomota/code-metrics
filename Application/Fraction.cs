4c6bbecb9309e616ba9ff61e385252b7f1527946
Reginaldo Mota <reginaldomotacc@gmail.com>
2025-12-31 13:48:03 -0300

    feat: melhorias na implementação da classe Fraction para permitir reusabilidade de código

--- Application/Fraction.cs
+++ Application/Fraction.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Application.CodeMetrics;
 
-public class Fraction
+public class Fraction : IParsable<Fraction>
 {
     public int Numerator { get; private set; }
     public int Denominator { get; private set; }
@@ -18,12 +20,7 @@ public class Fraction
 
     public double GetValue()
     {
-        return (double) Numerator / Denominator;
-    }
-
-    public bool IsLesser(Fraction other)
-    {
-        return GetValue() < other.GetValue();
+        return (double)Numerator / Denominator;
     }
 
     public bool IsLesser(double value)
@@ -31,18 +28,47 @@ public class Fraction
         return GetValue() < value;
     }
 
-    public bool IsGreater(Fraction other)
-    {
-        return GetValue() > other.GetValue();
-    }
-
     public bool IsGreater(double value)
     {
-        return GetValue() > value;
+        var getValue = GetValue();
+        bool isGreater = getValue > value;
+        return isGreater;
     }
 
     public override string ToString()
     {
         return $"{Numerator}/{Denominator}";
+    }
+
+    public static Fraction Parse(string s, IFormatProvider? provider)
+    {
+        string[] parts = s.Split('/');
+        if (parts.Length != 2)
+            throw new FormatException("Formato inválido! Use: numerador/denominador. Exemplo 1/2");
+
+        if (!int.TryParse(parts[0], out int numerator) || !int.TryParse(parts[1], out int denominator))
+            throw new FormatException("Valores inválidos! Digite números inteiros");
+
+        if (denominator == 0)
+            throw new ArgumentException("Denominador não pode ser zero");
+
+        return new Fraction().Of(numerator, denominator);
+    }
+
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Fraction result)
+    {
+        result = null;
+        if (s == null)
+            return false;
+
+        try
+        {
+            result = Parse(s, provider);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
