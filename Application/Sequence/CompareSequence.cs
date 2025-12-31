50d9a19926e5a0486660cb4e16c87d05c21c55d8
Reginaldo Mota <reginaldomotacc@gmail.com>
2025-12-31 13:51:58 -0300

    feat: melhorias no método de comparar sequencias para o modelo proposto, com ioc e reusabilidade

---
+++ Application/Sequence/CompareSequence.cs
@@ -0,0 +1,84 @@
+namespace Application.CodeMetrics;
+
+using System.Globalization;
+using Application.Sequence.Factories;
+using Application.Sequence.Resources;
+using Application.Sequence.Utils;
+
+public class CompareSequence
+{
+    private readonly IStopConditionFactory _stopConditionFactory;
+
+    public CompareSequence(IStopConditionFactory stopConditionFactory)
+    {
+        _stopConditionFactory = stopConditionFactory;
+    }
+
+    public void Execute()
+    {
+        List<double> sequenceDouble = ReadSequence<double>();
+        List<Fraction> sequenceFraction = ReadSequence<Fraction>();
+
+        if (sequenceDouble.Count == 0 || sequenceFraction.Count == 0)
+        {
+            Console.WriteLine(SequenceMessages.EmptySequencesError);
+            return;
+        }
+
+        List<Fraction> result = GetFractionsGreaterThanHalfOfA(sequenceDouble, sequenceFraction);
+
+        Console.WriteLine($"\n{SequenceMessages.Separator}");
+        Console.WriteLine(SequenceMessages.ResultTitle);
+        Console.WriteLine(SequenceMessages.Separator);
+
+        if (result.Count == 0)
+        {
+            Console.WriteLine(SequenceMessages.NoMatchingFractions);
+        }
+        else
+        {
+            foreach (var fraction in result)
+
+                Console.WriteLine($"{fraction} = {fraction.GetValue():F4}");
+        }
+    }
+
+    private List<Fraction> GetFractionsGreaterThanHalfOfA(List<double> sequenceA, List<Fraction> sequenceB)
+    {
+        int halfOfA = (sequenceA.Count + 1) / 2;
+
+        return sequenceB
+            .Where(fraction => sequenceA.Count(value => fraction.IsGreater(value)) > halfOfA)
+            .ToList();
+    }
+
+    private List<T> ReadSequence<T>() where T : IParsable<T>
+    {
+        List<T> sequence = new List<T>();
+        var typeName = typeof(T).Name;
+
+        Console.WriteLine(SequenceMessages.GetSequenceTitle(typeName));
+        Console.WriteLine(SequenceMessages.GetInputHint(typeName));
+
+        while (true)
+        {
+            Console.Write(SequenceMessages.InputPrompt);
+            if (T.TryParse(Console.ReadLine()?.NormalizeInput(), CultureInfo.InvariantCulture, out T? value))
+            {
+                if (_stopConditionFactory.Create<T>().IsStop(value))
+                {
+                    Console.WriteLine(SequenceMessages.ReadingFinished);
+                    break;
+                }
+
+                sequence.Add(value);
+            }
+            else
+            {
+                Console.WriteLine(SequenceMessages.GetInvalidInputMessage(typeName));
+            }
+        }
+
+        return sequence;
+    }
+}
